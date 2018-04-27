ce2c1ee500416d4cb9d4cd0f170690f91643c949
hyd309 <570637885@qq.com>
2018-04-27 18:16:47 +0800

    基础类

---
+++ CoreBackendApi/Common/HttpHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBackendApi.Common
+{
+    public class HttpHelper
+    {
+        CookieContainer cookie = new CookieContainer();
+        public string HttpPost(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
+            request.CookieContainer = cookie;
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(postDataStr);
+            myStreamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            response.Cookies = cookie.GetCookies(response.ResponseUri);
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+        public static string HttpPost(string url, string paramData, Dictionary<string, string> headerDic = null)
+        {
+            string result = string.Empty;
+            try
+            {
+                HttpWebRequest wbRequest = (HttpWebRequest)WebRequest.Create(url);
+                wbRequest.Method = "POST";
+                wbRequest.ContentType = "application/x-www-form-urlencoded";
+                wbRequest.ContentLength = Encoding.UTF8.GetByteCount(paramData);
+                if (headerDic != null && headerDic.Count > 0)
+                {
+                    foreach (var item in headerDic)
+                    {
+                        wbRequest.Headers.Add(item.Key, item.Value);
+                    }
+                }
+                using (Stream requestStream = wbRequest.GetRequestStream())
+                {
+                    using (StreamWriter swrite = new StreamWriter(requestStream))
+                    {
+                        swrite.Write(paramData);
+                    }
+                }
+                HttpWebResponse wbResponse = (HttpWebResponse)wbRequest.GetResponse();
+                using (Stream responseStream = wbResponse.GetResponseStream())
+                {
+                    using (StreamReader sread = new StreamReader(responseStream))
+                    {
+                        result = sread.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            { }
+
+            return result;
+        }
+
+        public static string HttpGet(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+        public static string HttpGet(string Url, Dictionary<string,string> headers)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            foreach (var item in headers)
+            {
+                request.Headers.Add(item.Key,item.Value);
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+    }
+}
---
+++ CoreBackendApi/Common/ResultMsg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBackendApi.Common
+{
+    public class ResultMsg
+    {
+        public int ResultCode { get; set; }
+        public string ErrorMsg { get; set; }
+        public string ResultTimes { get; set; }
+        public object Data { get; set; }
+    }
+}
